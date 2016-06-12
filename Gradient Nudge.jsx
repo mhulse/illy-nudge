b1db4dd24e0f8878bba552d973a033618abade8f
Micky Hulse <mickyhulse@gmail.com>
2016-06-11 22:19:07 -0700

    Working script!

--- Gradient Nudge.jsx
+++ Gradient Nudge.jsx
@@ -1,5 +1,5 @@
 /* jshint laxbreak:true, -W043 */
-/* globals app, $, BridgeTalk */
+/* globals app, $ */
 
 // jshint ignore:start
 #target illustrator
@@ -15,9 +15,11 @@ var NUDGE = (function($application, $helper, undefined) {
 	// Private variable container object:
 	var _private = {};
 	
-	_private.document = $application.activeDocument;
+	// Active filled object path selection:
+	_private.selection;
 	
-	_private.increment = 0.5; // Make sure always positive, not zero.
+	// Floating point number, used as “nudge” value for gradient fill:
+	_private.increment;
 	
 	/**
 	 * Create UI window.
@@ -28,9 +30,26 @@ var NUDGE = (function($application, $helper, undefined) {
 	
 	_private.createWindow = function(title) {
 		
-		var dialog = new Window(
+		var dialog;
+		var group1;
+		var group2;
+		var button1;
+		var panel1;
+		var text1;
+		var text2;
+		var text3;
+		var buttonW;
+		var buttonNW;
+		var buttonN;
+		var buttonNE;
+		var buttonE;
+		var buttonSE;
+		var buttonS;
+		var buttonSW;
+		
+		dialog = new Window(
 			'dialog',
-			title,
+			title
 			/*
 			[100, 100, 440, 440],
 			undefined,
@@ -47,68 +66,112 @@ var NUDGE = (function($application, $helper, undefined) {
 		dialog.margins = 5; // Horizontal spacing!
 		dialog.spacing = 5; // Vertical spacing!
 		
-		var group = dialog.add('group');
-		group.spacing = 0;
-		group.margins = 0;
+		group1 = dialog.add('group');
+		group1.spacing = 0;
+		group1.margins = 0;
 		
-		var buttonW = group.add(
+		buttonW = group1.add(
 			'button',
 			[0, 0, 30, 30],
 			'W'
 		);
 		
-		var buttonNW = group.add(
+		buttonNW = group1.add(
 			'button',
 			[0, 0, 30, 30],
 			'NW'
 		);
 		
-		var buttonN = group.add(
+		buttonN = group1.add(
 			'button',
 			[0, 0, 30, 30],
 			'N'
 		);
 		
-		var buttonNE = group.add(
+		buttonNE = group1.add(
 			'button',
 			[0, 0, 30, 30],
 			'NE'
 		);
 		
-		var buttonE = group.add(
+		buttonE = group1.add(
 			'button',
 			[0, 0, 30, 30],
 			'E'
 		);
 		
-		var buttonSE = group.add(
+		buttonSE = group1.add(
 			'button',
 			[0, 0, 30, 30],
 			'SE'
 		);
 		
-		var buttonS = group.add(
+		buttonS = group1.add(
 			'button',
 			[0, 0, 30, 30],
 			'S'
 		);
 		
-		var buttonSW = group.add(
+		buttonSW = group1.add(
 			'button',
 			[0, 0, 30, 30],
 			'SW'
 		);
 		
-		/*
-		W  x-1 y0
-		NW x-1 y-1
-		N  x0 y-1
-		NE x1 y-1
-		E  x1 y0
-		SE x1 y1
-		S  x0 y1
-		SW x-1 y1
-		*/
+		buttonReset = group1.add(
+			'button',
+			[0, 0, 30, 30],
+			'X'
+		);
+		
+		group2 = dialog.add('group');
+		group2.margins = 0;
+		group2.spacing = 5;
+		group2.orientation = 'row';
+		group2.alignChildren = 'left';
+		
+		panel1 = group2.add('panel');
+		panel1.orientation = 'row';
+		panel1.spacing = 5;
+		panel1.margins = 5;
+		
+		text1 = panel1.add(
+			'statictext',
+			undefined,
+			'Increment: '
+		);
+		
+		text2 = panel1.add(
+			'edittext',
+			undefined,
+			_private.increment
+		);
+		text2.characters = 5;
+		
+		text3 = panel1.add(
+			'statictext',
+			undefined,
+			'pts'
+		);
+		
+		button1 = group2.add('button', undefined, 'Close', {
+			name: 'ok'
+		});
+		
+		//     +y
+		//      |
+		// -x ––+–– +x
+		//      |
+		//     -y
+		//
+		//  W = x-1, y 0
+		// NW = x-1, y 1
+		//  N = x 0, y 1
+		// NE = x 1, y 1
+		//  E = x 1, y 0
+		// SE = x 1, y-1
+		//  S = x 0, y-1
+		// SW = x-1, y-1
 		
 		buttonW.onClick = function() {
 			
@@ -116,149 +179,153 @@ var NUDGE = (function($application, $helper, undefined) {
 			
 		};
 		
-		var group2 = dialog.add('group');
-		group2.margins = 0;
-		group2.spacing = 5;
-		
-		group2.orientation = 'row';
-		group2.alignChildren = 'left';
-		
-		var panel = group2.add('panel');
-		panel.orientation = 'row';
-		panel.spacing = 5;
-		panel.margins = 5;
-		
-		var statictext = panel.add(
-			'statictext',
-			undefined,
-			'Increment: '
-		);
-		
-		var edittext = panel.add(
-			'edittext',
-			undefined,
-			_private.increment
-		);
-		
-		edittext.characters = 5;
-		
-		edittext.onChange = function() {
+		buttonNW.onClick = function() {
 			
-			_private.increment = parseFloat(edittext.text);
+			_private.nudgeGradient({ x: -1, y: 1 }, _private.increment);
 			
 		};
 		
-		var statictext2 = panel.add(
-			'statictext',
-			undefined,
-			'pts'
-		);
+		buttonN.onClick = function() {
+			
+			_private.nudgeGradient({ x: 0, y: 1 }, _private.increment);
+			
+		};
 		
-		group2.add('button', undefined, 'Close', {
-			name: 'ok'
-		});
+		buttonNE.onClick = function() {
+			
+			_private.nudgeGradient({ x: 1, y: 1 }, _private.increment);
+			
+		};
 		
-		dialog.layout.layout();
+		buttonE.onClick = function() {
+			
+			_private.nudgeGradient({ x: 1, y: 0 }, _private.increment);
+			
+		};
 		
-		return dialog;
+		buttonSE.onClick = function() {
+			
+			_private.nudgeGradient({ x: 1, y: -1 }, _private.increment);
+			
+		};
+		
+		buttonS.onClick = function() {
+			
+			_private.nudgeGradient({ x: 0, y: -1 }, _private.increment);
+			
+		};
+		
+		buttonSW.onClick = function() {
+			
+			_private.nudgeGradient({ x: -1, y: -1 }, _private.increment);
+			
+		};
+		
+		buttonReset.onClick = function() {
+			
+			_private.resetGradient(true);
+			
+		};
+		
+		text2.onChange = function() {
+			
+			_private.increment = parseFloat(text2.text);
+			
+		};
+		
+		// Center and show the dialog window:
+		dialog.center();
+		dialog.show();
 		
 	};
 	
 	_private.nudgeGradient = function(direction, increment) {
 		
-		var doc = _private.document;
-		var ipath =  doc.selection[0];
-		var fillColor = ipath.fillColor;
-		var fillColorMatrix = fillColor.matrix;
-		var x = fillColorMatrix.mValueTX;
-		var y = fillColorMatrix.mValueTY;
-			
-			//alert(ipath.fillColor.matrix.mValueTX +  ' ' + ipath.fillColor.matrix.mValueTY)
-			// Before: 1,0,0,1,0,0
-			/*
-			$.writeln(
-				'Before: ',
-				fillColorMatrix.mValueA,
-				',',
-				fillColorMatrix.mValueB,
-				',',
-				fillColorMatrix.mValueC,
-				',',
-				fillColorMatrix.mValueD,
-				',',
-				fillColorMatrix.mValueTX,
-				',',
-				fillColorMatrix.mValueTY
-			);
-			$.writeln();
-			*/
+		var ipath = _private.selection;
+		var matrix = ipath.fillColor.matrix;
 		
-			ipath.filled = false; // remove previous gradient transformations, to be able to apply new ones ??
-			ipath.fillColor = fillColor; // re apply the same gradient color, without transformations.
-			ipath.fillOverprint = ipath.fillOverprint; // without this, the gradient won't move or scale, but it does rotate ??
+		// Reset gradient so we can translate it:
+		_private.resetGradient();
+		
+		// Transate the gradient fill:
+		ipath.translate(
+			_private.getTranslate(matrix.mValueTX, increment, direction.x), // deltaX
+			_private.getTranslate(matrix.mValueTY, increment, direction.y), // deltaY
+			false, // transformObjects
+			false, // transformFillPatterns
+			true, // transformFillGradents
+			false // transformStrokePattern
+		);
+		
+		// Redraw Illustrator so we can see the gradient update:
+		$application.redraw();
+		
+	};
+	
+	_private.resetGradient = function(redraw) {
+		
+		var ipath = _private.selection;
+		var fillColor = ipath.fillColor;
+		
+		// Remove previous gradient transformations, to be able to apply new ones:
+		ipath.filled = false;
+		// Reapply the same gradient color, without transformations:
+		ipath.fillColor = fillColor;
+		// Without this, the gradient won’t move or scale (but, oddly, it does rotate):
+		ipath.fillOverprint = ipath.fillOverprint;
+		
+		// Redraw Illustrator?
+		if (redraw) {
 			
-			ipath.translate(
-				_private.getTranslate(x, increment, direction.x), // deltaX
-				_private.getTranslate(y, increment, direction.y), // deltaY
-				false, // transformObjects
-				false, // transformFillPatterns
-				true, // transformFillGradents
-				false // transformStrokePattern
-			);
+			// Redraw Illustrator so we can see the gradient update:
+			$application.redraw();
 			
-			/*
-			// After: 1,0,0,1,-0.5,0.5
-			$.writeln(
-				'After: ',
-				fillColorMatrix.mValueA,
-				',',
-				fillColorMatrix.mValueB,
-				',',
-				fillColorMatrix.mValueC,
-				',',
-				fillColorMatrix.mValueD,
-				',',
-				fillColorMatrix.mValueTX,
-				',',
-				fillColorMatrix.mValueTY
-			);
-			*/
-			app.redraw();
+		}
 		
 	};
 	
 	_private.getTranslate = function(matrix, increment, direction) {
 		
+		// Default to matrix:
 		var result = matrix;
 		
-		//$.writeln(matrix, ' ', increment, ' ', direction);
+		//$helper.writeln(matrix, ' ', increment, ' ', direction);
 		//        0.5          0.5             -1
 		//        0.5          0.5              0
 		
+		// Ignore zero values:
 		if (direction !== 0) {
 			
-			result = ((Math.abs(matrix) + increment) * direction);
+			// Add increment to matrix in desired direction:
+			result = (result + (increment * direction));
 			
 		}
 		
 		return result;
 		
-	}
+	};
 	
 	// Script setup, if coming through the “front door”:
 	_private.init = function(title) {
 		
-		var dialog;
-		
 		// Open document(s)?
 		if ($application.documents.length > 0) {
 			
-			// Make dialog window:
-			dialog = _private.createWindow(title);
+			// Get user’s selection:
+			_private.selection = $application.activeDocument.selection[0];
 			
-			// Center and show the dialog window:
-			dialog.center();
-			dialog.show();
+			// Selection?
+			if (_private.selection) {
+				
+				// Make and show the dialog window:
+				_private.createWindow(title);
+				
+			} else {
+				
+				// Nope, let the user know what they did wrong:
+				alert('You must select a filled path object.');
+				
+			}
 			
 		} else {
 			
@@ -271,10 +338,13 @@ var NUDGE = (function($application, $helper, undefined) {
 	
 	// Public API:
 	return {
-		init: function(args) {
+		init: function(defaults) {
+			
+			// Initialize the default increment value:
+			_private.increment = defaults.increment;
 			
 			// First time through, fire-up the palette window:
-			_private.init(args.title);
+			_private.init(defaults.title);
 			
 		}
 	};
@@ -282,7 +352,8 @@ var NUDGE = (function($application, $helper, undefined) {
 })(app, $);
 
 NUDGE.init({
-	title: 'Nudge Gradient'
+	title: 'Nudge Gradient',
+	increment: 0.5
 });
 
 // Done!
--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# illy-nudge
-Nudge gradient centerpoint.
+# Illy Nudge
+
+**Nudge gradient centerpoint.**
+
+![](demo.gif)
+
+## Installation
+
+Use the below single-line Unix bash command to quickly install this script into your Illustrator `Scripts` folder.
+
+```bash
+bash <(curl -sL https://git.io/...)
+```
+
+Alternatively, you can dowload [`Gradient Nudge.jsx`](Gradient Nudge.jsx) into your Illustrator `Scripts` folder, located in the `/lllustrator CC/Presets` folder in your `lllustrator CC` installation directory.
+
+The script’s filename, minus the file extension, will appear in the Scripts menu.
+
+Run the script from the scripts menu in Illustrator.
+
+Voilà!
+
+## Usage
+
+![](example.png)
+
+1. Select a gradient-filled path object
+1. Open the script
+1. Click the directional buttons (W, NW, N, NE, E, SE, S, SW)
+1. Modify the increment value to suit your needs
+1. Click “Close” to exit
+
+## Thanks
+
+Many thanks to [Carlos Canto](https://forums.adobe.com/people/CarlosCanto) (<http://aiscripts.com/>) for the [scripting help](https://forums.adobe.com/message/8796411). :octocat:
+
+---
+
+[<img width="32" height="32" align="right" src="http://mirrors.creativecommons.org/presskit/icons/zero.png" title="CC0 1.0 Universal (CC0 1.0)" target="_blank">](https://creativecommons.org/publicdomain/zero/1.0/)
+
+To the extent possible under law, [@mhulse](https://github.com/mhulse) has waived all copyright and related or neighboring rights to this work.
+
+<img width="20" height="20" align="absmiddle" src="https://github.global.ssl.fastly.net/images/icons/emoji/octocat.png" alt=":octocat:" title=":octocat:" class="emoji">
